0c68aadfcf0fc1d8b119188ad7f18d4cfed4adf1
TonySeek <tonyseek@gmail.com>
2013-09-03 10:01:58 +0800

    initial commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+from brownant import __version__
+
+
+setup(
+    name="brownant",
+    version=__version__,
+    description="Brown Ant is a crawling framework.",
+    author="Jiangge Zhang",
+    author_email="tonyseek@gmail.com",
+    packages=find_packages(excludes=["tests"]),
+    install_requires=[
+        "Werkzeug >= 0.8",
+    ],
+)
