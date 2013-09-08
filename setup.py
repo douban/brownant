279926974f5002f3f8b6fc8092e332221d90d112
TonySeek <tonyseek@gmail.com>
2013-09-08 19:02:09 +0800

    fix the argument name typo.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Brown Ant is a crawling framework.",
     author="Jiangge Zhang",
     author_email="tonyseek@gmail.com",
-    packages=find_packages(excludes=["tests"]),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
         "Werkzeug >= 0.8",
     ],
