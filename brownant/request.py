111de4a6bdaf1f5f53afcd92f2cd0dc3275c1058
TonySeek <tonyseek@gmail.com>
2013-09-10 20:10:25 +0800

    add the raw URL into the request object.

--- brownant/request.py
+++ brownant/request.py
@@ -4,11 +4,13 @@ from __future__ import absolute_import, unicode_literals
 class Request(object):
     """The crawling request object.
 
+    :param url: the raw URL inputted from the dispatching app.
     :param args: the query arguments decoded from query string of the URL.
     """
 
-    def __init__(self, args):
+    def __init__(self, url, args):
+        self.url = url
         self.args = args
 
     def __repr__(self):
-        return "Request(args={self.args})".format(self=self)
+        return "Request(url={self.url}, args={self.args})".format(self=self)
