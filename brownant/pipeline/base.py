fa07b57371bd253e900e5740750775a9b36429ac
TonySeek <tonyseek@gmail.com>
2013-09-21 18:31:07 +0800

    complement the docs of the base pipeline property.

--- brownant/pipeline/base.py
+++ brownant/pipeline/base.py
@@ -2,7 +2,25 @@ from werkzeug.utils import cached_property
 
 
 class PipelineProperty(cached_property):
+    """The base class of pipeline properties.
 
+    There are three kinds of initial parameters.
+
+    The first kind of initial parameters is required attribute. If a keyword
+    argument's name was defined in :attr:`required_attrs`, it will be assigned
+    as a instance attribute.
+
+    The second kind is the member of :attr:`attr_names`, whose name always end
+    with `_attr`, such as `raw_html_attr`.
+
+    The last kind is option parameter, which will be placed at an instance
+    owned attribute named :attr:`options`. The subclasses could set default
+    option value in the :meth:`prepare`.
+
+    :param kwargs: the parameters with the three kinds.
+    """
+
+    #: the names of required attributes.
     required_attrs = set()
 
     def __init__(self, **kwargs):
@@ -11,7 +29,9 @@ class PipelineProperty(cached_property):
         self.__module__ = None
         self.__doc__ = None
 
+        #: the definition of attr_names
         self.attr_names = {}
+        #: the definition of options
         self.options = {}
 
         assigned_attrs = set()
@@ -34,8 +54,28 @@ class PipelineProperty(cached_property):
         self.prepare()
 
     def prepare(self):
-        pass
+        """This method will be called after instance ininialized. The
+        subclasses could override the implementation.
+
+        In general purpose, the implementation of this method should give
+        default value to options and the members of :attr:`attr_names`.
+
+        Example:
+
+        .. code-block:: python
+
+           def prepare(self):
+               self.attr_names.setdefault("raw_html_attr", "raw_html")
+               self.options.setdefault("use_proxy", False)
+        """
 
     def get_attr(self, obj, name):
+        """Get attribute of the target object with the configured attribute
+        name in the :attr:`attr_names` of this instance.
+
+        :param obj: the target object.
+        :param name: the internal name used in the :attr:`attr_names`.
+                     .e.g. `"raw_html_attr"`
+        """
         attr_name = self.attr_names[name]
         return getattr(obj, attr_name)
