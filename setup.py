fad4fbcb1b5c04da51aa4903ef4ab8f827d10a3e
TonySeek <tonyseek@gmail.com>
2013-09-29 05:40:38 +0800

    expand metadata.

--- setup.py
+++ setup.py
@@ -3,13 +3,33 @@ from setuptools import setup, find_packages
 from brownant import __version__
 
 
+with open("README.rst") as long_description_file:
+    long_description = long_description_file.read()
+
+
 setup(
     name="brownant",
+    packages=find_packages(exclude=["tests", "docs"]),
     version=__version__,
-    description="Brown Ant is a crawling framework.",
+    description="BrownAnt is a crawling framework",
+    long_description=long_description,
     author="Jiangge Zhang",
     author_email="tonyseek@gmail.com",
-    packages=find_packages(exclude=["tests"]),
+    url="https://tonyseek.github.io/brownant",
+    license="MIT",
+    keywords=["crawler", "crawling"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Environment :: Other Environment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     install_requires=[
         "Werkzeug >= 0.8",
         "lxml >= 3.1",
