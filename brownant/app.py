594a31acabdd5ca2e72bb13a9dcb6deec7590e22
TonySeek <tonyseek@gmail.com>
2013-11-25 03:32:22 +0800

    refactor: move the url cleaner into utility function.

--- brownant/app.py
+++ brownant/app.py
@@ -10,7 +10,7 @@ from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
 
 from .request import Request
 from .exceptions import NotSupported
-from .utils import to_bytes_safe
+from .utils import clean_up_url
 
 
 class Brownant(object):
@@ -42,43 +42,12 @@ class Brownant(object):
                   from the werkzeug bound URL map, the query_args is a
                   multidict from the werkzeug.
         """
-        url = urllib.parse.urlparse(url_string)
-        url = self.validate_url(url)
+        url = clean_up_url(urllib.parse.urlparse(url_string))
         url_adapter = self.url_map.bind(server_name=url.hostname,
                                         url_scheme=url.scheme,
                                         path_info=url.path)
         query_args = url_decode(url.query)
         return url, url_adapter, query_args
-
-    def validate_url(self, url):
-        """Validate the :class:`~urllib.parse.ParseResult` object.
-
-        This method will make sure the :meth:`~brownant.app.BrownAnt.parse_url`
-        could work as expected even meet a unexpected URL string.
-
-        :param url: the parsed url.
-        :type url: :class:`~urllib.parse.ParseResult`
-        """
-        # fix up the non-ascii path
-        url_path = to_bytes_safe(url.path)
-        url_path = urllib.parse.quote(url_path, safe=b"/%")
-
-        # fix up the non-ascii query
-        url_query = to_bytes_safe(url.query)
-        url_query = urllib.parse.quote(url_query, safe=b"?=&")
-
-        url = urllib.parse.ParseResult(url.scheme, url.netloc, url_path,
-                                       url.params, url_query, url.fragment)
-
-        # validate the components of URL
-        has_hostname = url.hostname is not None and len(url.hostname) > 0
-        has_http_scheme = url.scheme in ("http", "https")
-        has_path = not len(url.path) or url.path.startswith("/")
-
-        if not (has_hostname and has_http_scheme and has_path):
-            raise NotSupported("invalid url: %s" % repr(url))
-
-        return url
 
     def dispatch_url(self, url_string):
         """Dispatch the URL string to the target endpoint function.
--- brownant/utils.py
+++ brownant/utils.py
@@ -1,4 +1,10 @@
 from six import text_type
+from six.moves import urllib
+
+from .exceptions import NotSupported
+
+
+__all__ = ("to_bytes_safe", "clean_up_url")
 
 
 def to_bytes_safe(text, encoding="utf-8"):
@@ -20,3 +26,34 @@ def to_bytes_safe(text, encoding="utf-8"):
         return text.encode(encoding)
 
     return text
+
+
+def clean_up_url(url):
+    """Clean up the :class:`~urllib.parse.ParseResult` object.
+
+    This function will make sure the :meth:`~brownant.app.BrownAnt.parse_url`
+    could work as expected even meet a unexpected URL string.
+
+    :param url: the parsed url.
+    :type url: :class:`~urllib.parse.ParseResult`
+    """
+    # fix up the non-ascii path
+    url_path = to_bytes_safe(url.path)
+    url_path = urllib.parse.quote(url_path, safe=b"/%")
+
+    # fix up the non-ascii query
+    url_query = to_bytes_safe(url.query)
+    url_query = urllib.parse.quote(url_query, safe=b"?=&")
+
+    url = urllib.parse.ParseResult(url.scheme, url.netloc, url_path,
+                                   url.params, url_query, url.fragment)
+
+    # validate the components of URL
+    has_hostname = url.hostname is not None and len(url.hostname) > 0
+    has_http_scheme = url.scheme in ("http", "https")
+    has_path = not len(url.path) or url.path.startswith("/")
+
+    if not (has_hostname and has_http_scheme and has_path):
+        raise NotSupported("invalid url: %s" % repr(url))
+
+    return url
