9a9825907fa49d6f73c752a2dec67278b4cd60f6
TonySeek <tonyseek@gmail.com>
2014-02-10 18:30:58 +0800

    rename the base exception's name.

--- brownant/exceptions.py
+++ brownant/exceptions.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
 
-class BrownAntException(Exception):
-    """The base exception of the brown ant framework."""
+class BrownantException(Exception):
+    """The base exception of the Brownant framework."""
 
 
-class NotSupported(BrownAntException):
+class NotSupported(BrownantException):
     """The given URL or other identity is from a platform which not support.
 
     This exception means any url rules of the app which matched the URL could
